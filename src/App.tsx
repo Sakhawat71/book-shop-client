062eb3dbd6f7722f0ab9d307832b741c46b67e65
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-05 16:05:37 +0600

    add Tailwind CSS integration and update App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css'
 
 function App() {
 
-  return (
-    <div>
-      Book Shop
-    </div>
-  )
+    return (
+        <div className='bg-sky-500'>
+            <p className='text-red-500 text-2xl'>Hello</p>
+            <h2 className='text-red-300 text-3xl text-center'>Book Shop</h2>
+        </div>
+    )
 }
 
 export default App

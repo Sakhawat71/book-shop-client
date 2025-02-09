0f18aa28a9ee6247d515fab15c2444ca56a8ff22
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-09 19:24:54 +0600

    add routing for new pages; implement AboutUs, Dashboard, and Products components

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,24 +2,29 @@ import { createBrowserRouter } from "react-router";
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import MainLayout from "../components/layout/MainLayout";
+import Products from "../pages/Products/Products";
+import AboutUs from "../pages/AboutUs";
+import Dashboard from "../pages/Dashboard";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout />,
-    },
-    {
-        path: '/dashboard',
-        element: <MainLayout />,
-    },
-    {
-        path: '/products',
-        element: <MainLayout />,
-    },
-    {
-        path: '/about',
-        element: <MainLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard />,
+            },
+            {
+                path: '/products',
+                element: <Products />,
+            },
+            {
+                path: '/about',
+                element: <AboutUs />,
+            },
+        ]
     },
     {
         path: '/login',

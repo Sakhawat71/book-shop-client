6dbcec4661b412b12a110225e7abdc0c9300004b
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-02-13 19:57:08 +0600

    update build scripts and API base URL; add Vercel configuration for deployment

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 export const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://book-shop-server-71.vercel.app/api",
+    // baseUrl: "http://localhost:5000/api",
     credentials: "include",
     prepareHeaders(headers, { getState }) {
 
